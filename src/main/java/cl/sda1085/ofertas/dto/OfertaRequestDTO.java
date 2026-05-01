f9177cfc3b258504c0741128cc64b4c88db2241c
Gonza-2103 <ghormazabal.d@gmail.com>
2026-05-01 11:58:06 -0400

    Elaboración del 'repository'

--- src/main/java/cl/sda1085/ofertas/dto/OfertaRequestDTO.java
+++ src/main/java/cl/sda1085/ofertas/dto/OfertaRequestDTO.java
@@ -1,4 +1,26 @@
 package cl.sda1085.ofertas.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class OfertaRequestDTO {
+
+    @NotNull(message = "El monto es obligatorio.")
+    @Positive(message = "El monto debe ser mayor a cero.")
+    private BigDecimal monto;
+
+    @NotNull(message = "El ID del usuario es obligatorio.")
+    private Long idUsuario;
+
+    @NotNull(message = "El ID del producto es obligatorio.")
+    private Long idProducto;
 }
