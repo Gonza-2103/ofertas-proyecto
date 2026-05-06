5e6e249895b72b6c0bcdd79bcaa0966c97bc3633
Usuario <geral.gonzalezs@duocuc.cl>
2026-05-05 21:26:02 -0400

    Desarrollo OfertaController

--- src/main/java/cl/sda1085/ofertas/controller/OfertaController.java
+++ src/main/java/cl/sda1085/ofertas/controller/OfertaController.java
@@ -1,4 +1,58 @@
 package cl.sda1085.ofertas.controller;
 
+
+import cl.sda1085.ofertas.dto.OfertaRequestDTO;
+import cl.sda1085.ofertas.dto.OfertaResponseDTO;
+import cl.sda1085.ofertas.service.OfertaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ofertas")
+@RequiredArgsConstructor
+
 public class OfertaController {
+
+    private final OfertaService ofertaService;
+
+    @GetMapping
+    public ResponseEntity <List<OfertaResponseDTO>> obtener() {
+        return ResponseEntity.ok(ofertaService.obtenerTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity <OfertaResponseDTO> obtenerPorId(@PathVariable Long id) {
+        return ofertaService.obtenerPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<OfertaResponseDTO> crearOferta(
+           @Valid @RequestBody OfertaRequestDTO dto) {
+
+        return ResponseEntity.status(201).body(ofertaService.guardar(dto));
+
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OfertaResponseDTO> actualizar(
+            @PathVariable Long id,
+            @Valid @RequestBody OfertaRequestDTO dto) {
+
+        return ofertaService.actualizar(id, dto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>  eliminar(@PathVariable Long id) {
+        ofertaService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
