376e616780d450c93802c38de975303b15bdbebf
Usuario <geral.gonzalezs@duocuc.cl>
2026-05-17 22:57:26 -0400

    Registrar bean de WebClient.Builder en la clase principal

---
+++ src/main/java/cl/sda1085/ofertas/config/WebClientConfig.java
@@ -0,0 +1,13 @@
+package cl.sda1085.ofertas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
